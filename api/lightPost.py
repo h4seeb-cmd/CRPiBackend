c4797c0535c64f5e31a6167e272ad0ad423ac3ad
h4seeb-cmd <mirzahbeg123@gmail.com>
2023-06-04 13:31:09 -0700

    updating light api

--- api/lightPost.py
+++ api/lightPost.py
@@ -4,15 +4,23 @@ import traceback
 from flask import Blueprint, jsonify
 from flask_restful import Api, Resource
 from flask_cors import CORS
-from api.lightTest import getLightLvl
+from api.lightTest import LightSensor
+import time
 
 light_api = Blueprint('light_api', __name__, url_prefix='/api/light')
 api = Api(light_api)
 
-CORS(light_api, methods=['GET', 'POST'], origins='*')
+CORS(light_api, methods=['GET'], origins='*')
 
 class GetLight(Resource):
     def get(self):
-        return jsonify(getLightLvl)
+        sensor = LightSensor()
+        try:
+            while True:
+                return("Light Level : " + str(sensor.readLight()) + " lx")
+                time.sleep(10)
+        except KeyboardInterrupt:
+            pass
+
 
 api.add_resource(GetLight, '/get', endpoint='get')
