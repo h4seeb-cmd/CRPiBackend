04d08ffbbe0adec7c3a6415c363553bef016d845
Ananyag2617 <90804060+Ananyag2617@users.noreply.github.com>
2023-06-06 13:05:02 -0700

    Update weather.py

--- api/weather.py
+++ api/weather.py
@@ -11,3 +11,24 @@ class GetWeather(Resource):
     def get(self):
         message = "example msg"
         return message
+    
+class PostWeather(Resource):
+    def post(self):
+        endpoint = "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather"
+        body = request.get_json()
+        location = body.get("location")
+        query_param = body.get("query_param")
+        querystring = {query_param: location}
+        headers = {
+            "X-RapidAPI-Key": "9e76af0f32msh97501ce28ffe3b8p1c0da6jsnc3113faae226",
+            "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com"
+        }
+        try:
+            response = requests.get(endpoint, params=querystring, headers=headers)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return {"error": "Request failed"}, response.status_code
+        except Exception:
+            print(traceback.format_exc())
+            return {"error": "Internal server error"}, 500
